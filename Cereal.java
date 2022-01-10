74daadbde734aefe5674c9441580aec42f9ace3b
Venti <70521040+s11029568@users.noreply.github.com>
2022-01-10 09:17:50 -0500

    V0.1

--- Cereal.java
+++ Cereal.java
@@ -1,1 +1,15 @@
-// Maxx Secrest, 01/10/22, AP Data Lab, V0.0
+// Maxx Secrest, 01/10/22, AP Data Lab, V0.1
+
+public class Cereal
+{
+
+
+    private double Fiber;
+    private String CerealName;
+    private double Cups;
+    private double FiberPerCup;
+
+
+
+
+}
